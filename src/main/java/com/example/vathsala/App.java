98dd8ea3e5dcafa442fa9d668b95cc1f7bdfa28c
vathsalacit <vathsala.ise@cambridge.edu.in>
2025-04-22 12:31:22 +0530

    Initial commit with Jenkinsfile

---
+++ src/main/java/com/example/vathsala/App.java
@@ -0,0 +1,13 @@
+package com.example.vathsala;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
